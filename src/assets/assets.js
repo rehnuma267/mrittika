70f2dcf21e256c7473b7124c4c97f34515266d16
rehnuma267 <rehnumatarannum486@gmail.com>
2025-02-15 12:42:28 +0600

    navigation added

---
+++ src/assets/assets.js
@@ -0,0 +1,8 @@
+import blog_4 from "./blog_4.jpg";
+import search_icon from "./search_icon.png";
+import basket_icon from "./basket_icon.png";
+export const assets = {
+  blog_4,
+  search_icon,
+  basket_icon,
+};
